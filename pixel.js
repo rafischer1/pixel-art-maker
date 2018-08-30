2f7eed1760b53b9e5df0fc7fd929015e36d3fc77
Robert Fischer <artiefischer@gmail.com>
2018-08-30 17:14:51 -0600

    thursday 5:14pm

--- pixel.js
+++ pixel.js
@@ -5,20 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
   // set title
   document.querySelector('title').innerText = 'Pixel-Art Maker'
 
-  //let the parent element with the grid be the this
-  // the place holding the event listener
-  // addEventListener on each createSquare
-  // apply background-color: red;
-
   // declare container on which event listener is placed
   let paintBoard = document.querySelector('.pixel-board')
   // listener.classList.add = "pixelOff";
   // 
   // let pixels = document.querySelectorAll('.pixel')
-
   //pixel.classList.add = "pixelOff";
 
-  function paintPixel(event) {
+  function paintPixel(event, paint) {
     console.log("The event.target is: ", event.target)
     console.log('The this is:', this)
     let pixel = event.target
@@ -26,13 +20,25 @@ document.addEventListener("DOMContentLoaded", function() {
       pixel.classList.replace('pixelOff', 'pixelOn')
     } else {
       pixel.classList.replace('pixelOn', 'pixelOff')
-      console.log('toggling?');
     }
+
   }
 
   paintBoard.addEventListener('click', paintPixel)
 
+  // classList[0]
+  //   .add
+  //classlist as an object of the element classList key:0 value:"red"
 
+  function selectColor(event) {
+    console.log('Event target for brush:', event.target)
+    let paint = event.target[0]
+    console.log(span[0])
+
+  }
+  let palletteBoard = document.querySelector('.pallette-board')
+
+  palletteBoard.addEventListener('click', selectColor)
 
 
   // class is off go ahead and replace with class on}
@@ -41,50 +47,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
-
-
-
-  // find body
-  // let body = document.getElementsByTagName('body')[0]
-  // // make a container for the grid
-  // let container = document.createElement('div')
-  // container.classList.add('board')
-  // container.classList.add('container')
-  // body.appendChild(container)
-  // 
-  // const rowNum = 2
-  // for (var j = 0; j < rowNum; j++) {
-  //   container.appendChild(createRow(j))
-  // }
-  // 
-  // function createRow(j) {
-  //   // create a row
-  //   let row = document.createElement('div')
-  //   row.classList.add('row')
-  //   row.classList.add('no-gutters')
-  //   // add row to the body
-  //   //container.appendChild(row)
-  //   // loop to create columns
-  //   createColumns(row, j)
-  //   return row
-  // }
-  // 
-  // function createColumns(row, j) {
-  //   const colNum = 8
-  //   for (let i = 0; i < colNum; i++) {
-  //     // make the bootstrap column
-  //     let col = document.createElement('div')
-  //     col.classList.add('col')
-  //     col.appendChild(createSquare((i + j) % 2))
-  //     row.appendChild(col)
-  //   }
-  // }
-  // 
-  // function createSquare() {
-  //   let div = document.createElement('div')
-  //   div.classList.add('square')
-  // }
 
 
   // stay above here

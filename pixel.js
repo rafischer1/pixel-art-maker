ded89ab76969a7910f833d169f1007603c39c835
Robert Fischer <artiefischer@gmail.com>
2018-08-30 14:51:26 -0600

    Thursday 2:52pm

--- pixel.js
+++ pixel.js
@@ -1,47 +1,91 @@
-console.log("Chessboard is still here")
-// set title
-document.querySelector('title').innerText = 'Chess Board'
-// find body
-let body = document.getElementsByTagName('body')[0]
-// make a container for the grid
-let container = document.createElement('div')
-container.classList.add('board')
-container.classList.add('container')
-body.appendChild(container)
+document.addEventListener("DOMContentLoaded", function() {
+  // stay below here
 
-const rowNum = 8
-for (var j = 0; j < rowNum; j++) {
-  container.appendChild(createRow(j))
-}
+  console.log("The Script is working atleast...")
+  // set title
+  document.querySelector('title').innerText = 'Pixel-Art Maker'
 
-function createRow(j) {
-  // create a row
-  let row = document.createElement('div')
-  row.classList.add('row')
-  row.classList.add('no-gutters')
-  // add row to the body
-  //container.appendChild(row)
-  // loop to create columns
-  createColumns(row, j)
-  return row
-}
+  //let the parent element with the grid be the this
+  // the place holding the event listener
+  // addEventListener on each createSquare
+  // apply background-color: red;
 
-function createColumns(row, j) {
-  const colNum = 8
-  for (let i = 0; i < colNum; i++) {
-    // make the bootstrap column
-    let col = document.createElement('div')
-    col.classList.add('col')
-    col.appendChild(createSquare((i + j) % 2))
-    row.appendChild(col)
+  // declare container on which event listener is placed
+  let paintBoard = document.querySelector('.pixel-board')
+  // listener.classList.add = "pixelOff";
+  // 
+  // let pixels = document.querySelectorAll('.pixel')
+
+  //pixel.classList.add = "pixelOff";
+
+  function paintPixel(event) {
+    console.log("The event.target is: ", event.target)
+    console.log('The this is:', this)
+    let pixel = event.target
+    if (pixel.classList.contains('pixelOff')) {
+      pixel.classList.replace('pixelOff', 'pixelOn')
+    } else {
+      pixel.classList.replace('pixelOn', 'pixelOff')
+      console.log('toggling?');
+    }
   }
-}
 
-function createSquare(black = false) {
-  let div = document.createElement('div')
-  div.classList.add('square')
-  if (black) {
-    div.classList.add('black')
-  }
-  return div
-}
+  paintBoard.addEventListener('click', paintPixel)
+
+
+
+
+  // class is off go ahead and replace with class on}
+
+
+
+
+
+
+
+
+
+  // find body
+  // let body = document.getElementsByTagName('body')[0]
+  // // make a container for the grid
+  // let container = document.createElement('div')
+  // container.classList.add('board')
+  // container.classList.add('container')
+  // body.appendChild(container)
+  // 
+  // const rowNum = 2
+  // for (var j = 0; j < rowNum; j++) {
+  //   container.appendChild(createRow(j))
+  // }
+  // 
+  // function createRow(j) {
+  //   // create a row
+  //   let row = document.createElement('div')
+  //   row.classList.add('row')
+  //   row.classList.add('no-gutters')
+  //   // add row to the body
+  //   //container.appendChild(row)
+  //   // loop to create columns
+  //   createColumns(row, j)
+  //   return row
+  // }
+  // 
+  // function createColumns(row, j) {
+  //   const colNum = 8
+  //   for (let i = 0; i < colNum; i++) {
+  //     // make the bootstrap column
+  //     let col = document.createElement('div')
+  //     col.classList.add('col')
+  //     col.appendChild(createSquare((i + j) % 2))
+  //     row.appendChild(col)
+  //   }
+  // }
+  // 
+  // function createSquare() {
+  //   let div = document.createElement('div')
+  //   div.classList.add('square')
+  // }
+
+
+  // stay above here
+})
